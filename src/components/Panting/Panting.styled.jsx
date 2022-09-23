7de0722e1cb0a20a5b3eb4a1def33a167e4ee5a6
Nikolay Tankov <tankov.n.k@gmail.com>
2022-09-23 18:27:22 +0300

    function color

--- src/components/Panting/Panting.styled.jsx
+++ src/components/Panting/Panting.styled.jsx
@@ -6,20 +6,43 @@ export const Container = styled.div`
 
 // функция управления локальным ПРОПС на элементе разметки
 // ====================================================
+
+// вариант с выносом условия в функцию
+const setColor = props => {
+  switch (props.a) {
+    case true:
+      // color green
+      return 'rgb(16, 154, 55)';
+    // color red
+    case false:
+      return 'rgb(218, 22, 22)';
+
+    // на всякий случай дефолтный цвет
+    default:
+      return 'rgb(218, 22, 22)';
+  }
+};
+// ====================================================
+
 export const Title = styled.h2`
   font-size: 48px;
-
-  color: ${props => {
-    switch (props.a) {
-      case true:
-        // color green
-        return 'rgb(16, 154, 55)';
-
-      case false:
-        return 'rgb(218, 22, 22)';
-    }
-  }};
+  color: ${setColor};
 `;
+
+// color: ${props => {
+//     switch (props.a) {
+//       case true:
+//         // color green
+//         return 'rgb(16, 154, 55)';
+//       // color red
+//       case false:
+//         return 'rgb(218, 22, 22)';
+
+//       // на всякий случай дефолтный цвет
+//       default:
+//         return 'rgb(218, 22, 22)';
+//     }
+//   }};
 
 // ${props => {
 //     console.log(props.type);
