1a76736259075011e4cb930ae3ba5b3aa537109b
Nikolay Tankov <tankov.n.k@gmail.com>
2022-09-25 18:05:06 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// обязательно для ШАБЛОНЫ и потом обертка ThemeProvider
 import { ThemeProvider } from '@emotion/react';
 import { App } from 'components/App/App';
 import './index.css';
