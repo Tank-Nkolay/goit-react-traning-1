eaaeffacbcef9d812fbc6cb9636271ca4ee9c2b8
Nikolay Tankov <tankov.n.k@gmail.com>
2022-09-27 08:43:09 +0300

    222

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,13 +2,27 @@ import React from 'react';
 
 // пример компонент класса
 class Counter extends React.Component {
+  handleIncrimente = () => {
+    console.log('We cliked more');
+    console.log(this);
+  };
+
   render() {
     return (
       <div>
         <span>0</span>
         <div>
-          <button type="button">Увеличить на 1</button>
-          <button type="button">Уменьшить на 1</button>
+          <button type="button" onClick={this.handleIncrimente}>
+            Увеличить на 1
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              console.log('We cliked less');
+            }}
+          >
+            Уменьшить на 1
+          </button>
         </div>
       </div>
     );
@@ -16,3 +30,9 @@ class Counter extends React.Component {
 }
 
 export default Counter;
+
+// вешаем на кнопку обработчик событий
+// обработчик встроенная функция РЕАКТ
+// синтаксис onСобытие
+// варианты собыий в ДОКАХ
+// onClick, onSubmit, onChange, onMouseNove
