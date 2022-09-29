e0a42dc615f6aa343d6cf8ef551431b9dc8e27aa
Nikolay Tankov <tankov.n.k@gmail.com>
2022-09-29 17:19:39 +0300

    222

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -2,15 +2,26 @@ import React from 'react';
 import { Box, Card, Button } from './ColorPicker.styled';
 
 class ColorPicker extends React.Component {
+  state = {
+    activeOptionsIdx: 3,
+  };
+
   render() {
     return (
       <Box>
         <h2>Color picker</h2>
         {/* this.props.options достукиваемся до ПРОПС */}
         <Card>
-          {this.props.options.map(({ label, color }) => (
-            <Button key={label} style={{ backgroundColor: color }}></Button>
-          ))}
+          {this.props.options.map(({ label, color }, index) => {
+            return (
+              <Button
+                key={label}
+                style={{
+                  backgroundColor: color,
+                }}
+              ></Button>
+            );
+          })}
         </Card>
       </Box>
     );
@@ -18,3 +29,15 @@ class ColorPicker extends React.Component {
 }
 
 export default ColorPicker;
+
+// выделение елемента по его index
+/* <Button
+key={label}
+style={{
+  backgroundColor: color,
+  border:
+    index === this.state.activeOptionsIdx
+      ? '5px solid black'
+      : 'none',
+}}
+></Button>  */
