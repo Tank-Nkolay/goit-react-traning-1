27002f0c8b3d75bf4a3022fceff8fc3600289043
Nikolay Tankov <tankov.n.k@gmail.com>
2022-09-30 18:26:55 +0300

    111

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -4,23 +4,33 @@ import { Container, Li, Text, Button } from './ToDoList.styled';
 class ToDoList extends React.Component {
   state = {
     todos: [
-      { id: 'id-1', text: 'дело-1', completed: false },
+      { id: 'id-1', text: 'дело-1', completed: true },
       { id: 'id-2', text: 'дело-2', completed: false },
       { id: 'id-3', text: 'дело-3', completed: false },
       { id: 'id-4', text: 'дело-4', completed: false },
-      { id: 'id-5', text: 'дело-5', completed: false },
+      { id: 'id-5', text: 'дело-5', completed: true },
     ],
   };
   // ======================================================
+  // метод удаления задач
   onDeleteTodo = todoId => {
     this.setState(prevState => ({
       todos: prevState.todos.filter(todo => todo.id !== todoId),
     }));
   };
+  //   метод показать законченные задачи true
+  completedTodos = this.state.todos.reduce(
+    (acc, todo) => (todo.completed ? acc + 1 : acc),
+    0
+  );
   // ======================================================
   render() {
     return (
       <Container>
+        <div>
+          <p>Общее количество задач: {this.state.todos.length}</p>
+          <p>Количество выполненных задач: {this.completedTodos}</p>
+        </div>
         {this.state.todos.map(({ id, text }) => (
           <Li key={id}>
             <Text>{text}</Text>
