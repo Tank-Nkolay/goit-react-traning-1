8aa8153ef1583b8309baa8956c4666b57afc92b7
Nikolay Tankov <tankov.n.k@gmail.com>
2022-10-10 20:00:36 +0300

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -66,7 +66,7 @@ export class App extends React.Component {
             Открыть модалку
           </button>
           {showModal && (
-            <Modal>
+            <Modal onClose={this.togleModal}>
               <h1>Hello!</h1>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -7,6 +7,21 @@ const modalRoot = document.querySelector('#modal-root');
 export default class Modal extends Component {
   state = {};
 
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      console.log('Close');
+      this.props.onClose();
+    }
+  };
+
   render() {
     return createPortal(
       <div>
