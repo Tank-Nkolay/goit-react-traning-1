858087714de7e7f0b91a6f8ffda9baa8aa27f21e
Nikolay Tankov <tankov.n.k@gmail.com>
2022-10-12 19:31:09 +0300

    styled pokemon

--- src/components/Pokemon/PokemonForm.js
+++ src/components/Pokemon/PokemonForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ImSearch } from 'react-icons/im';
+import { Container } from './PokemonForm.styled';
 
 class PokemonForm extends React.Component {
   state = {
@@ -12,25 +13,27 @@ class PokemonForm extends React.Component {
 
   handleSubmit = e => {
     e.preventDefault();
-
     this.props.onFormSubmit(this.state.pokemonName);
     this.setState({ pokemonName: '' });
   };
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          name="pokemonName"
-          value={this.state.pokemonName}
-          onChange={this.handleNameChange}
-        />
-        <button type="submit">
-          <ImSearch style={{ marginRight: 8 }} />
-          Найти
-        </button>
-      </form>
+      <Container>
+        <h2>FORM POKEMON</h2>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="pokemonName"
+            value={this.state.pokemonName}
+            onChange={this.handleNameChange}
+          />
+          <button type="submit">
+            <ImSearch style={{ marginRight: 8 }} />
+            Найти
+          </button>
+        </form>
+      </Container>
     );
   }
 }
---
+++ src/components/Pokemon/PokemonForm.styled.jsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  outline: 3px solid red;
+  padding: 60px;
+  padding-top: 20px;
+`;
