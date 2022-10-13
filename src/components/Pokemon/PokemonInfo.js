eb576923db170fe10ad266b5e126691fd3476fa8
Nikolay Tankov <tankov.n.k@gmail.com>
2022-10-13 08:01:39 +0300

    pokemon null

--- src/components/Pokemon/PokemonInfo.js
+++ src/components/Pokemon/PokemonInfo.js
@@ -4,27 +4,38 @@ class PokemonInfo extends React.Component {
   state = {
     pokemon: null,
     loading: false,
+    error: null,
   };
 
   componentDidUpdate(prevProps, prevState) {
     const prevName = prevProps.pokemonName;
     const nextName = this.props.pokemonName;
     if (prevName !== nextName) {
-      console.log('Изменилось имя покемона');
-      this.setState({ loading: true });
+      //   console.log('Изменилось имя покемона');
+      // включаем ЛОАДЕР, обнуляем ПОКЕМОН
+      this.setState({ loading: true, pokemon: null });
       fetch(`https://pokeapi.co/api/v2/pokemon/${nextName}`)
-        .then(res => res.json())
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          //   так мы обрабатываем 404 ошибку
+          return Promise.reject(new Error(`Нет покемона с именем ${nextName}`));
+        })
         .then(pokemon => this.setState({ pokemon }))
+        .catch(error => this.setState({ error }))
         .finally(() => this.setState({ loading: false }));
+      console.log(this.error);
     }
   }
 
   render() {
-    const { pokemon, loading } = this.state;
+    const { pokemon, loading, error } = this.state;
     const { pokemonName } = this.props;
     return (
       <div>
         <h2>Pokemon Info</h2>
+        {error && <h2>{error.message}</h2>}
         {loading && <h2>Загружаем ...</h2>}
         {!pokemonName && <div>Введите имя покемона</div>}
         {pokemon && (
