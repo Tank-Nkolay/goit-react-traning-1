2190c57aa720413885c1b3610da4f3d0c1a5d121
Nikolay Tankov <tankov.n.k@gmail.com>
2022-10-13 06:42:09 +0300

    update

--- src/components/Pokemon/PokemonInfo.js
+++ src/components/Pokemon/PokemonInfo.js
@@ -3,6 +3,7 @@ import React from 'react';
 class PokemonInfo extends React.Component {
   state = {
     pokemon: null,
+    loading: false,
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -10,19 +11,23 @@ class PokemonInfo extends React.Component {
     const nextName = this.props.pokemonName;
     if (prevName !== nextName) {
       console.log('Изменилось имя покемона');
+      this.setState({ loading: true });
       fetch(`https://pokeapi.co/api/v2/pokemon/${nextName}`)
         .then(res => res.json())
-        .then(pokemon => this.setState({ pokemon }));
+        .then(pokemon => this.setState({ pokemon }))
+        .finally(() => this.setState({ loading: false }));
     }
   }
 
   render() {
+    const { pokemon, loading } = this.state;
+    const { pokemonName } = this.props;
     return (
       <div>
         <h2>PokemonInfo</h2>
-        {this.state.pokemon && (
-          <div>Тут будет покемон: {this.state.pokemon.name}</div>
-        )}
+        {loading && <h2>Загружаем ...</h2>}
+        {!pokemonName && <div>Введите имя покемона</div>}
+        {pokemon && <div>Тут будет покемон: {pokemon.name}</div>}
       </div>
     );
   }
