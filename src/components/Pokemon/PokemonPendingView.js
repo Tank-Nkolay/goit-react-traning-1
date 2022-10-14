9e5f7828dfc183dfa2b2fb28691488608e79f1c0
Nikolay Tankov <tankov.n.k@gmail.com>
2022-10-14 10:42:07 +0300

    http done

--- src/components/Pokemon/PokemonPendingView.js
+++ src/components/Pokemon/PokemonPendingView.js
@@ -1,13 +1,27 @@
 import { ImSpinner } from 'react-icons/im';
+// элемент СКЕЛЕТОНА
+import PokemonDataView from './PokemonDataView';
+import waitImg from '../../jpg/waitImg.png';
 
 // для СПИНЕР стили прописали в index.css и вызвали здесь
 function PokemonPendingView({ pokemonName }) {
+  // элемент СКЕЛЕТОНА (для предварительного дефолтного показа)=======
+  const pokemon = {
+    name: pokemonName,
+    sprites: { other: { 'official-artwork': { front_default: waitImg } } },
+    stats: [],
+  };
+  // ==========================
+
   return (
     <div>
       <div>
         <ImSpinner size="32" className="icon-spin" />
-        Загружаем ... {pokemonName}
+        Загружаем ...
       </div>
+      {/* элемент СКЕЛЕТОНА======= */}
+      <PokemonDataView pokemon={pokemon} />
+      {/* ======================= */}
     </div>
   );
 }
