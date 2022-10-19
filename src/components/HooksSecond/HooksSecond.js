7e762f5d09a5f552e6f581ee12649496df7b31e7
Nikolay Tankov <tankov.n.k@gmail.com>
2022-10-19 20:52:42 +0300

    1

--- src/components/HooksSecond/HooksSecond.js
+++ src/components/HooksSecond/HooksSecond.js
@@ -1,42 +1,77 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 // import { Container } from './Hooks.styled';
 
-export default class SignupForm extends Component {
-  state = {
-    email: '',
-    password: '',
-  };
+export default function SignupForm() {
+  // передаем начальное значение
+  const [email, setEmail] = useState('');
 
-  handleChange = evt => {
-    const { name, value } = evt.target;
-    this.setState({ [name]: value });
-  };
+  return (
+    <form autoComplete="off">
+      <label>
+        <span>Почта</span>
+        <input
+          type="email"
+          name="email"
+          onChange={this.handleChange}
+          value={email}
+        />
+      </label>
 
-  render() {
-    return (
-      <form autoComplete="off">
-        <label>
-          <span>Почта</span>
-          <input
-            type="email"
-            name="email"
-            onChange={this.handleChange}
-            value={this.state.email}
-          />
-        </label>
+      <label>
+        <span>Пароль</span>
+        <input
+          type="password"
+          name="password"
+          onChange={this.handleChange}
+          value={this.state.password}
+        />
+      </label>
 
-        <label>
-          <span>Пароль</span>
-          <input
-            type="password"
-            name="password"
-            onChange={this.handleChange}
-            value={this.state.password}
-          />
-        </label>
-
-        <button type="submit">Зарегистрироваться</button>
-      </form>
-    );
-  }
+      <button type="submit">Зарегистрироваться</button>
+    </form>
+  );
 }
+
+// =====================================================
+// старый вариант на классах
+// =====================================================
+// export default class SignupForm extends Component {
+//   state = {
+//     email: '',
+//     password: '',
+//   };
+
+//   handleChange = evt => {
+//     const { name, value } = evt.target;
+//     this.setState({ [name]: value });
+//   };
+
+//   render() {
+//     return (
+//       <form autoComplete="off">
+//         <label>
+//           <span>Почта</span>
+//           <input
+//             type="email"
+//             name="email"
+//             onChange={this.handleChange}
+//             value={this.state.email}
+//           />
+//         </label>
+
+//         <label>
+//           <span>Пароль</span>
+//           <input
+//             type="password"
+//             name="password"
+//             onChange={this.handleChange}
+//             value={this.state.password}
+//           />
+//         </label>
+
+//         <button type="submit">Зарегистрироваться</button>
+//       </form>
+//     );
+//   }
+// }
+// =====================================================
