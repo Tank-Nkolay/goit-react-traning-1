eb4ed5b6616b365cac9adfcdb04a8df834f0400f
Nikolay Tankov <tankov.n.k@gmail.com>
2022-10-20 18:29:54 +0300

    color picker hooks

--- src/components/HooksSecond/HooksColorPicker.js
+++ src/components/HooksSecond/HooksColorPicker.js
@@ -5,20 +5,19 @@ import { Box, Card, Button } from './HooksColorPicker.styled';
 function HooksColorPicker({ options }) {
   const [activeOptionIdx, setActiveOptionIdx] = useState(0);
   // деструкторизируем label и берем его из props по значению индекс
-  const { label } = options[activeOptionIdx].label;
+  const { label } = options[activeOptionIdx];
 
   return (
     <Box>
       <h2>Color picker</h2>
       <p>Выбран цвет: {label}</p>
       <Card>
-        {this.props.options.map(({ label, color }, index) => (
+        {options.map(({ label, color }, index) => (
           <Button
             key={label}
             style={{
               backgroundColor: color,
-              transform:
-                index === this.state.activeOptionsIdx ? 'scale(1.2)' : 'none',
+              transform: index === activeOptionIdx ? 'scale(1.2)' : 'none',
             }}
             onClick={() => setActiveOptionIdx(index)}
           ></Button>
