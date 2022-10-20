aad5f4465e8e02e6ac077ac46ce0312ad862c794
Nikolay Tankov <tankov.n.k@gmail.com>
2022-10-20 22:29:13 +0300

    1

--- src/components/HooksSecond/HooksFormClean.js
+++ src/components/HooksSecond/HooksFormClean.js
@@ -1,66 +1,66 @@
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
-// // выносим в отдельную локальную функцию общуюю логику для многих случаев
-// const useLocalStorage = (key, defaultValue) => {
-//   const [state, setState] = useState(() => {
-//     return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
-//   });
+// выносим в отдельную локальную функцию общуюю логику для многих случаев
+const useLocalStorage = (key, defaultValue) => {
+  const [state, setState] = useState(() => {
+    return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
+  });
 
-//   return [state, setState];
-// };
+  return [state, setState];
+};
 
-// // обьявляем эту функцию и передаем туда значения
-// export default function SignupFormClean() {
-//   const [email, setEmail] = useLocalStorage('email', '');
-//   const [password, setPassword] = useLocalStorage('password', '');
+// обьявляем эту функцию и передаем туда значения
+export default function SignupFormClean() {
+  const [email, setEmail] = useLocalStorage('email', '');
+  const [password, setPassword] = useLocalStorage('password', '');
 
-//   // функция - что должен сделать setEmail - записать текущее значение в email
-//   const handleChange = event => {
-//     const { name, value } = event.target;
-//     switch (name) {
-//       case 'email':
-//         setEmail(value);
-//         break;
-//       case 'password':
-//         setPassword(value);
-//         break;
-//       default:
-//         return;
-//     }
-//   };
+  // функция - что должен сделать setEmail - записать текущее значение в email
+  const handleChange = event => {
+    const { name, value } = event.target;
+    switch (name) {
+      case 'email':
+        setEmail(value);
+        break;
+      case 'password':
+        setPassword(value);
+        break;
+      default:
+        return;
+    }
+  };
 
-//   // записываем данные в ЛОКАЛ-СТОРЕДЖ при изменении значения email (пр этом email переводим в строку)
-//   useEffect(() => {
-//     window.localStorage.setItem('email', JSON.stringify(email));
-//   }, [email]);
+  // записываем данные в ЛОКАЛ-СТОРЕДЖ при изменении значения email (пр этом email переводим в строку)
+  useEffect(() => {
+    window.localStorage.setItem('email', JSON.stringify(email));
+  }, [email]);
 
-//   useEffect(() => {
-//     window.localStorage.setItem('password', JSON.stringify(password));
-//   }, [password]);
+  useEffect(() => {
+    window.localStorage.setItem('password', JSON.stringify(password));
+  }, [password]);
 
-//   return (
-//     <form autoComplete="off">
-//       <label>
-//         <span>Почта</span>
-//         <input
-//           type="email"
-//           name="email"
-//           onChange={handleChange}
-//           value={email}
-//         />
-//       </label>
+  return (
+    <form autoComplete="off">
+      <label>
+        <span>Почта</span>
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={email}
+        />
+      </label>
 
-//       <label>
-//         <span>Пароль</span>
-//         <input
-//           type="password"
-//           name="password"
-//           onChange={handleChange}
-//           value={password}
-//         />
-//       </label>
+      <label>
+        <span>Пароль</span>
+        <input
+          type="password"
+          name="password"
+          onChange={handleChange}
+          value={password}
+        />
+      </label>
 
-//       <button type="submit">Зарегистрироваться</button>
-//     </form>
-//   );
-// }
+      <button type="submit">Зарегистрироваться</button>
+    </form>
+  );
+}
