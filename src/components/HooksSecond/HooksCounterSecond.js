a3815f763e97ab8ccbf6125b7e667d8077e2b67b
Nikolay Tankov <tankov.n.k@gmail.com>
2022-10-29 12:12:59 +0300

    1

--- src/components/HooksSecond/HooksCounterSecond.js
+++ src/components/HooksSecond/HooksCounterSecond.js
@@ -1,26 +1,50 @@
-import { useState, useEffect } from 'react';
+import { useReducer } from 'react';
 import { Box } from './HooksCounterSecond.styled';
 
-export default function HooksCounterSecond() {
-  const [counter, setCounter] = useState(0);
+function countReducer(prevState, nextState) {
+  return prevState + nextState;
+}
 
-  //   задаем логику, предыдущее состояние + или - 1
-  const handleCounterIncrement = () => {
-    setCounter(state => state + 1);
-  };
-  const handleCounterDecrement = () => {
-    setCounter(state => state - 1);
-  };
+export default function HooksCounterSecond() {
+  const [count, setCount] = useReducer(countReducer, 0);
 
   return (
     <Box>
-      <p>Значение = {counter}</p>
-      <button type="button" onClick={handleCounterIncrement}>
+      <p>Значение = {count}</p>
+      <button type="button" onClick={() => setCount(1)}>
         Увеличить на 1
       </button>
-      <button type="button" onClick={handleCounterDecrement}>
+      <button type="button" onClick={() => setCount(-1)}>
         Уменьшить на 1
       </button>
     </Box>
   );
 }
+
+// =============================================================
+// ВАРИАНТ ДО РЕФАКТОРИНГА
+// =============================================================
+// export default function HooksCounterSecond() {
+//       const [counter, setCounter] = useState(0);
+
+//     //   задаем логику, предыдущее состояние + или - 1
+//     const handleCounterIncrement = () => {
+//       setCounter(state => state + 1);
+//     };
+//     const handleCounterDecrement = () => {
+//       setCounter(state => state - 1);
+//     };
+
+//     return (
+//       <Box>
+//         <p>Значение = {counter}</p>
+//         <button type="button" onClick={handleCounterIncrement}>
+//           Увеличить на 1
+//         </button>
+//         <button type="button" onClick={handleCounterDecrement}>
+//           Уменьшить на 1
+//         </button>
+//       </Box>
+//     );
+//   }
+// =============================================================
